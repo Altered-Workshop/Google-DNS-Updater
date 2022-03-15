1264623aaa0edb4327eef884b7dba8e950ef2a67
Altered-Workshop <60989281+Altered-Workshop@users.noreply.github.com>
2022-03-15 13:41:49 -0700

    Delete secrets.sh

--- secrets.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Google DNS/Domain Hosting Secrets
-Google_DNS_User="1NTOVFNIlpF7J3Wb" #Google provided username for DDNS
-Google_DNS_Pass="gkWX3HrFG9avM0Nw" #Google provided password for DDNS
-Google_Domain="saccullocloud.com" #Domain name
-
-#Email Secrets
-eMail="anthony.saccullo@icloud.com"
