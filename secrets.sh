2eebb5348ee1df4077844a13e1e1dcad2ded91bf
Raspberry-Medic <60989281+Altered-Workshop@users.noreply.github.com>
2022-03-15 13:41:10 -0700

    Update secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -1,2 +1,9 @@
-these are secrets
-yes
+#! /bin/bash
+
+# Google DNS/Domain Hosting Secrets
+Google_DNS_User="1NTOVFNIlpF7J3Wb" #Google provided username for DDNS
+Google_DNS_Pass="gkWX3HrFG9avM0Nw" #Google provided password for DDNS
+Google_Domain="saccullocloud.com" #Domain name
+
+#Email Secrets
+eMail="anthony.saccullo@icloud.com"
